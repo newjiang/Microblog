3b92865af1780164f578cb21948117ee00118d55
newjiang <510146714@qq.com>
2018-05-23 13:47:09 +0800

    解决搜索微博的图片问题

--- app/src/main/java/com/example/jiang/microblog/view/search/adapter/SearchNineImgAdapter.java
+++ app/src/main/java/com/example/jiang/microblog/view/search/adapter/SearchNineImgAdapter.java
@@ -5,12 +5,15 @@ package com.example.jiang.microblog.view.search.adapter;
  */
 
 import android.content.Context;
-import android.util.Log;
+import android.content.Intent;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.example.jiang.microblog.utils.IntentKey;
+import com.example.jiang.microblog.view.activity.ShowPictureActivity;
 import com.jaeger.ninegridimageview.NineGridImageViewAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,12 +28,20 @@ public class SearchNineImgAdapter extends NineGridImageViewAdapter<String> {
 
     @Override
     protected boolean onItemImageLongClick(Context context, ImageView imageView, int index, List<String> list) {
-        Log.e("SearchNineImgAdapter", list.toString());
         return super.onItemImageLongClick(context, imageView, index, list);
     }
 
     @Override
     protected void onItemImageClick(Context context, ImageView imageView, int index, List<String> list) {
-
+        ArrayList<String> stringList = new ArrayList<>();
+        for (String url : list) {
+            String square = "http:" + url;
+            String bmiddle = square.replace("square", "bmiddle");
+            stringList.add(bmiddle);
+        }
+        Intent intent = new Intent(context, ShowPictureActivity.class);
+        intent.putStringArrayListExtra(IntentKey.MICROBLOG_PICTURE, stringList);
+        intent.putExtra(IntentKey.MICROBLOG_PICTURE_NUMBER, index);
+        context.startActivity(intent);
     }
 }
